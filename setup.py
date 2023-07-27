f96dc7f33667587861952b516d00f952c0767b48
MrKein <luquealanbri2@gmail.com>
2023-07-27 18:50:19 -0300

    v1.0 clases cliente, producto y pedido

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name= "paquete_cliente_producto",
+    version= "1.0",
+    description= "este paquete tiene para calculo matematico",
+    author= "Alan",
+    author_email= "emaiñ@email.com",
+    packages= ["paquete1"] # carpetas en formato lista del paquete
+)
